c94b3c3879700b8c4c81daf77104b2e563456359
Ashwin_Kumar_H_M <Ashwin_Kumar_H_M@WN7X64-BPPNQ12.blr.apac.dell.com>
2017-01-27 16:31:24 +0530

    First Commit

---
+++ gitpjt1/src/com/pckg/MainClass.java
@@ -0,0 +1,17 @@
+package com.pckg;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		display();
+		Sales sales = new Sales();
+		sales.display();
+	}
+
+	private static void display() {
+		// TODO Auto-generated method stub
+		System.out.println("Main");
+	}
+
+}
---
+++ gitpjt1/src/com/pckg/Sales.java
@@ -0,0 +1,9 @@
+package com.pckg;
+
+public class Sales {
+	
+	public void display() {
+		System.out.println("Sales");
+	}
+
+}
